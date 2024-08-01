90b46b9e1b9896371fd6061d373b710ef2ddc075
Derk <derk@shotstack.io>
2024-08-01 22:18:38 +1000

    Update to 0.4.0

---
+++ demos/react/src/lib/shotstack/ShotstackContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const ShotstackContext = createContext();
+
+export const ShotstackProvider = ({ children }) => {
+  const [shotstack, setShotstack] = useState(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://js.shotstack.io/studio/0.4.0/shotstack.min.js';
+    script.async = true;
+    script.onload = () => {
+      if (window.shotstack) {
+        setShotstack({
+          ...window.shotstack,
+          load: json => window.shotstack.load('studio-sdk-editor', json),
+        });
+      }
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return <ShotstackContext.Provider value={shotstack}>{children}</ShotstackContext.Provider>;
+};
+
+export const useShotstack = () => {
+  return useContext(ShotstackContext);
+};
--- demos/vue/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import ShotstackPlugin from './plugins/shotstack';
-
-const app = createApp(App);
-app.use(ShotstackPlugin);
-app.mount('#app');
